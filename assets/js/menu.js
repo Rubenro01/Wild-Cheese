2d9ba4fa03d23a616d5ee16e9a9c13fd81a5ac1f
Rubenro01 <148970958+Rubenro01@users.noreply.github.com>
2025-05-20 01:44:52 +0200

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('header');
-  header?.querySelector('.menu-toggle')
-    ?.addEventListener('click', () => header.classList.toggle('active'));
-});
